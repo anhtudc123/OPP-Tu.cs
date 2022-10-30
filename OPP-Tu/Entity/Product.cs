9de708a89332caa7dc1c3f1dc970d638fe64b0a3
anhtudc123 <116538416+anhtudc123@users.noreply.github.com>
2022-10-30 22:42:00 +0700

    Add files via upload

---
+++ OPP-Tu/Entity/Product.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConsoleApp3.Entity;
+
+namespace b4product
+{
+    public class Product:BaseIdAndName
+    {
+        public int categoryId { get; set; }
+    }
+}
